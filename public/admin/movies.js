fbef56b428652d0628f05cca92ae00054ccafc8d
Glitch (movies-management-system) <none>
2022-04-11 15:48:51 +0000

    Initial commit

---
+++ public/admin/movies.js
@@ -0,0 +1,9 @@
+function next() 
+{
+   event.preventDefault();
+  console.log("it's working");
+  window.location = "/admin/staff.html";
+}
+
+// function should display error if date is out of range input
+// this is not complete
